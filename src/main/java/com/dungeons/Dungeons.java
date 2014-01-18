f5d021a32516d2751664b0c8a96b53ff476d63a8
Blackvein <alex_360_live@hotmail.com>
2014-01-17 22:03:54 -0800

    Boiler Plate

---
+++ src/main/java/com/dungeons/Dungeons.java
@@ -0,0 +1,25 @@
+package com.dungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * <p>TODO: Main plugin file</p>
+ *
+ */
+public class Dungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+        //TODO: onEnable logic
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+        //TODO: onDisable logic
+        
+    }
+    
+}
