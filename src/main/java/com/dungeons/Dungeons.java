8cb2aa87850d9a3296f043e365dbffedde075a89
Blackvein <alex_360_live@hotmail.com>
2014-01-17 22:39:45 -0800

    Added  configuration loading, listener registering.

--- src/main/java/com/dungeons/Dungeons.java
+++ src/main/java/com/dungeons/Dungeons.java
@@ -1,24 +1,66 @@
 package com.dungeons;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * <p>TODO: Main plugin file</p>
+ * <p>Represents the main Dungeons plugin file.</p>
  *
  */
 public class Dungeons extends JavaPlugin {
 
+    public static final Logger LOG = Logger.getLogger("Minecraft");
+    
+    //Enables/Disables error logging during most startup/loading procedures
+    public static boolean debug = true;
+    
     @Override
     public void onEnable() {
         
-        //TODO: onEnable logic
+        loadConfig();
+        this.getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+        LOG.info("[Dungeons] Version " + this.getDescription().getVersion() + " Enabled.");
         
     }
     
     @Override
     public void onDisable() {
         
-        //TODO: onDisable logic
+        LOG.info("[Dungeons] Version " + this.getDescription().getVersion() + " Disabled.");
+        
+    }
+    
+    /**
+     * <p>Loads the configuration from file.</p>
+     * 
+     */
+    private void loadConfig(){
+        
+        FileConfiguration config = new YamlConfiguration();
+        
+        try{
+            
+            config.load(this.getResource("config.yml"));
+            
+            /**
+             * debug is initialized to true in case there is a problem reading the
+             * config itself, so that the error will print out.
+             * 
+             * When reading however, it's defaulted to false if no value is specified.
+             */
+            debug = config.getBoolean("debug", false);
+            
+        }catch (IOException | InvalidConfigurationException ex){
+            
+            LOG.severe("[Dungeons] An error occurred reading the config file.");
+            if(debug)
+                ex.printStackTrace();
+            
+        }
         
     }
     
---
+++ src/main/java/com/dungeons/PlayerListener.java
@@ -0,0 +1,21 @@
+package com.dungeons;
+
+import org.bukkit.event.Listener;
+
+/**
+ *
+ * <p>Represents the handler for various Player Events.</p>
+ * 
+ * @author Blackvein
+ */
+public class PlayerListener implements Listener {
+    
+    private final Dungeons plugin;
+    
+    public PlayerListener(Dungeons plugin){
+        
+        this.plugin = plugin;
+        
+    }
+    
+}
